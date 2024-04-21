62a63abd6ad8ffa91dffd86df1f28c64b6c8a5e3
vnkenbi <vnkenbi@gmail.com>
2024-04-21 23:09:58 +0700

    tao thay doi dieu huong

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 
 import './App.scss';
 import Header from './components/Header/Header';
-import { Link } from 'react-router-dom'; //them
+import { Link, Outlet } from 'react-router-dom'; //them
 
 const App = () => {
 
 
   return (
     <div className="app-container">
+      <div className='header-container'>
+        <Header />
+      </div>
+      <div className='main-container'>
+        <div className='sidenav-container'>
 
-      <Header />
-
-      <div>
-        test Link
-        <div>
-          <button>
-            <Link to="/Users">Goto user Page</Link>
-          </button>
-          <button>
-            <Link to="/Admins">Goto Admin Page</Link>
-          </button>
+        </div>
+        <div className='app-content'>
+          <Outlet />
         </div>
       </div>
+
+
+
 
 
     </div>
