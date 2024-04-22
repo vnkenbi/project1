8c0d2e42be4b1afec78dd0d4a588200da3a79e5b
vnkenbi <vnkenbi@gmail.com>
2024-04-22 15:34:43 +0700

    Css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,13 +24,15 @@ const Header = () => {
           </Nav>
 
           <Nav>
-            <NavDropdown title="Setting" id="basic-nav-dropdown">
+            <button className='btn-login'>Log in</button>
+            <button className='btn-signup'>Sign up</button>
+            {/* <NavDropdown title="Setting" id="basic-nav-dropdown">
               <NavDropdown.Item >Log in</NavDropdown.Item>
               <NavDropdown.Item >Log out</NavDropdown.Item>
               <NavDropdown.Item >Profile</NavDropdown.Item>
               <NavDropdown.Divider />
 
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
 
         </Navbar.Collapse>
--- src/components/Home/Homepage.js
+++ src/components/Home/Homepage.js
@@ -8,6 +8,19 @@ const HomePage = (props) => {
                 <source src={videoHomepage}
                     type="video/mp4" />
             </video>
+            <div className='homepage-content'>
+                <div className='title-1'>Make forms
+                    worth filling out
+                </div>
+                <div className='title-2'>
+                    et more data—like signups, feedback, and anything else—with forms designed to be refreshingly different.
+                </div>
+                <div className='title-3'>
+                    <button>
+                        Get started—it's free
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
